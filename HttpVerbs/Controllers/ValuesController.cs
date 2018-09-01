f52bcb2d7a43c40bf483cf09322dd9a070a4bc68
shrutisinghal <shrutis@thoughtworks.com>
2018-09-02 00:04:59 +0530

    Added a new user model and patch http verb with required api calls (Requested APIs through PostMan/Curl)

--- HttpVerbs/Controllers/ValuesController.cs
+++ HttpVerbs/Controllers/ValuesController.cs
@@ -2,44 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HttpVerbs.Controllers
+namespace HttpVerbs
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        Dictionary<int,User> Users = new Dictionary<int,User>{
+            {1, new User(1, "a", "a@a.com")} ,
+            {2, new User(2, "b", "b@b.com")}
+        };
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<Dictionary<int,User>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Users;
         }
 
-        // GET api/values/5
+        // GET api/values/1
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<User> Get(int id)
         {
-            return "value";
+            return Users[id];
         }
 
         // POST api/values
+        // Request Body {"userId": 3, "userName": "c", "userEmail": "c@c.com"}
         [HttpPost]
-        public void Post([FromBody] string value)
+        public StatusCodeResult Post(User value)
         {
+            Users.Add(3, value);
+            return new StatusCodeResult(200);
         }
 
-        // PUT api/values/5
+        // PUT api/values/1
+        // Request Body {"userId": 1, "userName": "aa", "userEmail": "aa@a.com"}
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public StatusCodeResult Put(int id, User value)
         {
+            Users[id] = value;
+            return new OkResult();
         }
 
-        // DELETE api/values/5
+        // DELETE api/values/1
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        //PATCH api/values/1
+        // Request Body [{"op" : "replace", "path" : "/userEmail", "value" : "c@c.com"}]
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, JsonPatchDocument<User> value)
+        {
+            value.ApplyTo(Users[id]);
+            return new StatusCodeResult(201);
         }
     }
 }
---
+++ HttpVerbs/Model/User.cs
@@ -0,0 +1,27 @@
+﻿namespace HttpVerbs
+{
+    public class User
+    {
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public int UserId
+        {
+            get;
+            set;
+        }
+        public string UserEmail
+        {
+            get;
+            set;
+        }
+        public User(int Id, string Name, string Email)
+        {
+            UserId = Id;
+            UserName = Name;
+            UserEmail = Email;
+        }
+    }
+}
